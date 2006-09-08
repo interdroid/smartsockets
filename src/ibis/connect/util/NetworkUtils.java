06a780c87a0c1da90e5aab865a2b57b35fefef1b
jason <jason@aaf88347-d911-0410-b711-e54d386773bb>
2006-09-08 16:12:40 +0000

    Initial version of MAC address retrieval using exec('ifconfig'). Seems to work well on the VU and Grid5000, but currently only supports linux.
    git-svn-id: http://gforge.cs.vu.nl/svn/ibis/new-connect/trunk@4278 aaf88347-d911-0410-b711-e54d386773bb

--- src/ibis/connect/util/NetworkUtils.java
+++ src/ibis/connect/util/NetworkUtils.java
@@ -1,13 +1,20 @@
 package ibis.connect.util;
 
+import ibis.connect.util.net.NativeNetworkConfig;
+import ibis.connect.util.net.NetworkInfo;
+
 import java.io.IOException;
 import java.net.Inet4Address;
 import java.net.InetAddress;
 import java.net.NetworkInterface;
 import java.net.SocketException;
+import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.Enumeration;
 import java.util.List;
+import java.util.StringTokenizer;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.apache.log4j.Logger;
 
@@ -19,6 +26,7 @@ public class NetworkUtils {
     protected static Logger logger = 
         ibis.util.GetLogger.getLogger(NetworkUtils.class.getName());
     
+     
     private NetworkUtils() {
         // No instance allowed 
     }
@@ -219,33 +227,15 @@ public class NetworkUtils {
         
         return result.toString();        
     }
-        
+            
     /**
      * Converts a IP address to a String. 
      * 
      * @param ad the address to convert
      * @return the String representation of the address.
      */
-    public static String ipToString(InetAddress ad) { 
-        
-        return ad.getHostAddress(); 
-        
-        /*
-        
-        byte [] bytes = ad.getAddress();
-        
-        StringBuffer result = new StringBuffer("");
-        
-        for (int i=0;i<bytes.length;i++) { 
-            result.append(bytes[i] & 0xff);
-            
-            if (i != bytes.length-1) { 
-                result.append(".");
-            }
-        }
-        
-        return result.toString();
-        */        
+    public static String ipToString(InetAddress ad) {         
+        return ad.getHostAddress();                
     }
     
     /**
@@ -270,7 +260,89 @@ public class NetworkUtils {
         
         return result.toString();        
     }
-
+    
+    /**
+     * Converts a String containing a MAC address to bytes. 
+     * A 6 byte MAC address using single character seperators is expected as 
+     * input.    
+     * 
+     * @param mac the address to convert
+     * @return the byte [] representation of the address.
+     */        
+    public static byte [] MACStringToBytes(String mac) { 
+        
+        byte [] result = new byte[6];
+        
+        StringBuffer tmp = new StringBuffer(mac); 
+        
+        for (int i=0;i<6;i++) {             
+            result[i] = 
+                (byte) (0xff & Integer.parseInt(tmp.substring(i*3, i*3+2), 16));            
+        }
+        
+        return result;
+    }
+    
+    /**
+     * Converts an array of bytes to a String. The byte values are sepated 
+     * by a user-specified seperator. If required, the bytes are printed in 
+     * hexadecimal form.  
+      *
+     * @param b the bytes to convert
+     * @param hex specifies if a hexadecimal format is required.     
+     * @param sep the seperator to use
+     *  
+     * @return the String representation of the address.
+     */        
+    private static String bytesToString(byte [] b, boolean hex, char sep) { 
+     
+        StringBuffer tmp = new StringBuffer(); 
+        
+        for (int i=0;i<b.length;i++) {             
+            if (hex) {             
+                int value = 0xff & b[i]; 
+                
+                if (value <= 0xf) {
+                    tmp.append('0');
+                }
+                
+                tmp.append(Integer.toHexString(value));
+            } else { 
+                tmp.append(0xff & b[i]);
+            }
+            
+            if (i != b.length-1) { 
+                tmp.append(sep);
+            } 
+        }
+        
+        return tmp.toString();
+    }
+    
+    /**
+     * Converts an array of bytes containing a MAC address to a String. 
+     * A 6 byte MAC address is expected as input. A colon will be used as a 
+     * seperator.    
+     * 
+     * @param mac the address to convert
+     * @return the String representation of the address.
+     */        
+    public static String MACToString(byte [] mac) {
+        return bytesToString(mac, true, ':');
+    }
+    
+    /**
+     * Converts an array of bytes containing a IPv4 address, IPv4 broadcast 
+     * address or a netmask to a String. 
+     * 
+     * @param ipv4 the array to convert
+     * @return the String representation of the address or netmask.
+     */        
+    public static String ipv4ToString(byte [] ipv4) {
+        return bytesToString(ipv4, false, '.');
+    }
+    
+    
     public static boolean matchAddress(byte [] ad, byte [] sub, byte [] mask) { 
         
         if (sub.length != ad.length) {
@@ -304,140 +376,105 @@ public class NetworkUtils {
         }
     }
     
+    public static String getMACAddressAsString(InetAddress ip) throws IOException {         
+        return MACToString(getMACAddress(ip));
+    }
     
+    public static String getNetmaskAsString(InetAddress ip) throws IOException { 
+        return ipv4ToString(getNetmask(ip));
+    }
     
-    /*
+    public static String getBroadAsString(InetAddress ip) throws IOException { 
+        return ipv4ToString(getBroadcast(ip));
+    }
     
-    
-    
-    
-    
-    
-    public static InetAddress canConnectFrom(InetAddress adr) { 
-        
-        // Find the local address that is most likely to be able to connect to 
-        // the given address.
-        return canConnectFrom(adr, false); 
+    public static byte [] getMACAddress(InetAddress ip) throws IOException { 
+        return NativeNetworkConfig.getMACAddress(ip);
     }
 
-    private static int getDistance(InetAddress remote, InetAddress local) { 
-        
-        if (remote.equals(local)) {                 
-            return 0;
-        } 
-        
-        byte [] remoteIP = remote.getAddress();
-        byte [] localIP = local.getAddress();
-        
-        if (remoteIP.length != localIP.length) {
-            // For now don't mix IPv4 and IPv6
-            return Integer.MAX_VALUE;
-        }
-    
-        int distance = remoteIP.length;
-        
-        for (int i=0;i<remoteIP.length-1;i++) { 
-            if (remoteIP[i] == localIP[i]) { 
-                distance--;
-            } else {                 
-                // check if we founf any similarities 
-                if (distance != remoteIP.length) {
-                    return distance;
-                } 
-                
-                // no similarities. Return a score based on the type of the 
-                // local address. This ensures that is no similarities are found 
-                // at all, the addresses will stil be sorted in a decent order 
-                // [public, sitelocal, linklocal, loopback]
-                    
-                if (local.isLoopbackAddress()) {
-                    return Integer.MAX_VALUE;
-                }
-                if (local.isLinkLocalAddress()) {
-                    return Integer.MAX_VALUE/2;
-                }
-                
-                if (local.isSiteLocalAddress()) {
-                    return Integer.MAX_VALUE/3;
-                }
-                
-                // else: public address
-                return Integer.MAX_VALUE/4;
-            }
-        }    
-        
-        return distance;
+    public static byte [] getNetmask(InetAddress ip) throws IOException { 
+        return NativeNetworkConfig.getNetmask(ip);
     }
     
-    public static InetAddress [] possibleConnections(InetAddress adr) {
+    public static byte [] getBroadcast(InetAddress ip) throws IOException { 
+        return NativeNetworkConfig.getBroadcast(ip);
+    }
     
-        // Find the local address that is most likely to be able to connect to 
-        // the given address. The mayTry parameter specifies if this method is 
-        // allowed to create a connection to 'try' if it works...
+    
+    public static void main(String [] args) { 
         
-        InetAddress [] ias = getAllHostAddresses();
-                    
-        int [] distance = new int[ias.length];
-        Arrays.fill(distance, Integer.MAX_VALUE); 
+        ArrayList nwl = getNetworkInterfacesList();
         
-        for (int i=0;i<ias.length;i++) {         
-            distance[i] = getDistance(adr, ias[i]);
-        }
-        
-        // simple bubble sort 
-        for (int i=0;i<ias.length-1;i++) { 
-         
-            int best = i;
+        for (int i=0;i<nwl.size();i++) {             
+            NetworkInterface nwi = (NetworkInterface) nwl.get(i);
             
-            for (int j=i+1;j<ias.length;j++) {                 
-                if (distance[j] < distance[best]) { 
-                    best = j;
+            System.out.println("Found network interface: " 
+                    + nwi.getDisplayName()); 
+            
+            ArrayList ips = new ArrayList(); 
+            
+            getAllHostAddresses(nwi, ips, false, true);
+            
+            String mac = null;
+            
+            for (int j=0;j<ips.size();j++) {               
+                try { 
+                    mac = getMACAddressAsString((InetAddress) ips.get(j));
+                } catch (Exception e) {
+                    // ignore
+                }
+                
+                if (mac != null) { 
+                    break;
                 }
             }
-            
-            if (best != i) { 
-                // found a better one, so swap
-                InetAddress t1 = ias[i];
-                ias[i] = ias[best];
-                ias[best] = t1;
-                
-                int t2 = distance[i];
-                distance[i] = distance[best];
-                distance[best] = t2;                                
-            }            
-        }    
+
+            System.out.println("  MAC Address    : " + (mac != null ? mac : "unknown"));  
                        
-        return ias;        
-    } 
-        
-    public static InetAddress canConnectFrom(InetAddress adr, boolean mayTry) { 
-        
-        // Find the local address that is most likely to be able to connect to 
-        // the given address. The mayTry parameter specifies if this method is 
-        // allowed to create a connection to 'try' if it works...
-        
-        InetAddress [] ias = getAllHostAddresses();
-            
-        InetAddress best = null; 
-        int distance = Integer.MAX_VALUE;
-        
-        for (int i=0;i<ias.length;i++) { 
-        
-            int dist = getDistance(adr, ias[i]);
+            for (int j=0;j<ips.size();j++) {
+
+                InetAddress ip = (InetAddress) ips.get(j);
                 
-            if (dist < distance) { 
-                distance = dist;
-                best = ias[i];
+                if (ip instanceof Inet4Address) { 
+                
+                    String netmask = null;
+                
+                    try { 
+                        netmask = getNetmaskAsString(ip);
+                    } catch (Exception e) {
+                        // ignore
+                    }
+                
+                    String broadcast = null;
+                
+                    try { 
+                        broadcast = getBroadAsString(ip);
+                    } catch (Exception e) {
+                        // ignore
+                    }
+                    
+                    System.out.println("  IPv4 Address   : " + ipToString(ip));
+                    System.out.println("       Netmask   : " + (netmask != null ? netmask : "unknown"));
+                    System.out.println("       Broadcast : " + (broadcast != null ? broadcast : "unknown"));
+                } else { 
+                    System.out.println("  IPv6 Address   : " + ipToString(ip)); 
+                }
+                
+                String type = "global";
+                
+                if (ip.isLoopbackAddress()) { 
+                    type = "loopback";
+                } else if (ip.isLinkLocalAddress()) { 
+                    type = "link local";                        
+                } else if (ip.isSiteLocalAddress()) { 
+                    type = "site local";
+                } else if (ip.isMulticastAddress()) { 
+                    type = "multicast";
+                } 
+                    
+                System.out.println("       Type      : " + type);
+                System.out.println();
             }
-        }
-        
-        if (distance != Integer.MAX_VALUE) { 
-            return best;
-        } else { 
-            return null;
-        }
+        }         
     }
-    
-    */
-      
 }
