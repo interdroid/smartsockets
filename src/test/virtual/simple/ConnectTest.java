0242edec43c3db702fb6e58e61ed2e253b1e6d27
jason <jason@aaf88347-d911-0410-b711-e54d386773bb>
2006-11-27 21:27:16 +0000

    git-svn-id: http://gforge.cs.vu.nl/svn/ibis/new-connect/trunk@4757 aaf88347-d911-0410-b711-e54d386773bb

--- src/test/virtual/simple/ConnectTest.java
+++ src/test/virtual/simple/ConnectTest.java
@@ -7,6 +7,7 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.Random;
 
+import smartsockets.direct.SocketAddressSet;
 import smartsockets.virtual.VirtualServerSocket;
 import smartsockets.virtual.VirtualSocket;
 import smartsockets.virtual.VirtualSocketAddress;
@@ -22,49 +23,38 @@ public class ConnectTest {
     
     private static HashMap connectProperties;
     
+    private static Random rand = new Random();
+    
     public static void connect(VirtualSocketAddress target) { 
         
-        Random rand = new Random();
-        
-        for (int i=0;i<REPEAT;i++) {
-           
-            /*
-            int sleep = rand.nextInt(15000);
-            
-            try { 
-                Thread.sleep(sleep);
-            } catch (Exception e) {
-                // TODO: handle exception
-            }
-            */
-            
-            long time = System.currentTimeMillis();
-            
-            try { 
-                VirtualSocket s = sf.createClientSocket(target, TIMEOUT, 
-                        connectProperties);
-            
-                time = System.currentTimeMillis() - time;
-                        
-                System.out.println("Created connection to " + target + " in " + 
-                        time + " ms.");
 
-                DataInputStream in = new DataInputStream(s.getInputStream());
-                DataOutputStream out = new DataOutputStream(s.getOutputStream());
 
-                System.out.println("Server says: " + in.readUTF());
-                            
-                out.writeUTF("Hello server!");
-                out.flush();
-                   
-                VirtualSocketFactory.close(s, out, in);
-            } catch (Exception e) {
-                time = System.currentTimeMillis() - time;
+        long time = System.currentTimeMillis();
 
-                System.out.println("Failed to create connection to " + target + 
-                        " after " + time + " ms.");
-                e.printStackTrace();
-            }
+        try { 
+            VirtualSocket s = sf.createClientSocket(target, TIMEOUT, 
+                    connectProperties);
+
+            time = System.currentTimeMillis() - time;
+
+            System.out.println("Created connection to " + target + " in " + 
+                    time + " ms.");
+
+            DataInputStream in = new DataInputStream(s.getInputStream());
+            DataOutputStream out = new DataOutputStream(s.getOutputStream());
+
+            System.out.println("Server says: " + in.readUTF());
+
+            out.writeUTF("Hello server!");
+            out.flush();
+
+            VirtualSocketFactory.close(s, out, in);
+        } catch (Exception e) {
+            time = System.currentTimeMillis() - time;
+
+            System.out.println("Failed to create connection to " + target + 
+                    " after " + time + " ms.");
+            e.printStackTrace();
         }
     }
     
@@ -108,19 +98,50 @@ public class ConnectTest {
         connectProperties = new HashMap();
 
         int targets = args.length;
-                
+        int repeat = REPEAT;        
+        boolean sleep = false;
+        
         for (int i=0;i<args.length;i++) { 
             if (args[i].equals("-cache")) {                 
                 connectProperties.put("cache.winner", null);
                 args[i] = null;
                 targets--;
-            }
+            } else if (args[i].equals("-repeat")) { 
+                repeat = Integer.parseInt(args[i+1]);
+                args[i+1] = null;
+                args[i] = null;
+                targets -= 2;
+                i++;
+            } else if (args[i].equals("-sleep")) { 
+                sleep = true;
+                args[i] = null;
+                targets--;
+            } 
         }
 
+        VirtualSocketAddress [] targetAds = new VirtualSocketAddress[targets];
+        int index = 0;
+        
+        for (int i=0;i<args.length;i++) { 
+            if (args[i] != null) { 
+                targetAds[index++] = new VirtualSocketAddress(args[i]); 
+            }
+        } 
+        
         if (targets > 0) {             
-            for (int i=0;i<args.length;i++) {                
-                if (args[i] != null) {                 
-                    connect(new VirtualSocketAddress(args[i]));
+            
+            for (int r=0;r<REPEAT;r++) {        
+                for (VirtualSocketAddress a : targetAds) { 
+                    
+                    if (sleep) { 
+                        try { 
+                            Thread.sleep(1000 + rand.nextInt(5000));
+                        } catch (Exception e) {
+                            // TODO: handle exception
+                        }
+                    }
+                    
+                    connect(a);
                 }
             }
         } else {
