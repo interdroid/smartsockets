316c8a259b35cb916596cb089687f05d8d34f2f8
jason <jason@aaf88347-d911-0410-b711-e54d386773bb>
2007-02-27 09:01:30 +0000

    git-svn-id: http://gforge.cs.vu.nl/svn/ibis/new-connect/trunk@5096 aaf88347-d911-0410-b711-e54d386773bb

---
+++ src/smartsockets/virtual/InitializationException.java
@@ -0,0 +1,14 @@
+package smartsockets.virtual;
+
+public class InitializationException extends Exception {
+
+    private static final long serialVersionUID = 2144447287143637263L;
+
+    public InitializationException(String message) { 
+        super(message);
+    }
+    
+    public InitializationException(String message, Throwable cause) { 
+        super(message, cause);
+    }    
+}
